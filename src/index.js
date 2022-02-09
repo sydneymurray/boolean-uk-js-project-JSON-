18b33961e2595edbd6167fe7d0b8c9c2a5a320f1
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-02-09 01:59:29 +0000

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(8080, () => {
+server.listen(3030, () => {
   console.log("JSON Server is running");
 });
